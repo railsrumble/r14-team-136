84e7d5709b5b886f6407b7f8093ab0e9166a028e
Vivek Sampara <ravensnowbird@gmail.com>
2014-10-18 18:49:06 +0530

    testing with sqlite 3 dependency on heroku

--- app/controllers/surge_management_controller.rb
+++ app/controllers/surge_management_controller.rb
@@ -1,6 +1,20 @@
 class SurgeManagementController < ApplicationController
   layout "surge"
 
+  before_action :set_folder_paths
+
   def index
+
+  end
+
+  private
+
+  def set_folder_paths
+    p session[:id]
+    if Rails.env.production?
+      session[:id]
+    else
+
+    end
   end
 end
--- surge.gemspec
+++ surge.gemspec
@@ -20,4 +20,6 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", "~> 4.1.6"
 
   s.add_dependency 'sass-rails'
+
+  s.add_development_dependency "sqlite3"
 end
