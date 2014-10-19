166cb95ab5938657e71040177f42fc75858db292
Rafeeq Shaikh <rafeeq.sk786@gmail.com>
2014-10-19 20:52:54 +0530

    completed model generation and destroy

--- test/dummy/db/schema.rb
+++ test/dummy/db/schema.rb
@@ -11,6 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141019130136) do
+ActiveRecord::Schema.define(version: 20141019150105) do
+
+  create_table "notes", force: true do |t|
+    t.integer  "c1"
+    t.string   "c2"
+    t.text     "c3"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
